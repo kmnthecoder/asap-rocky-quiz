b21984d4b7a8c187700b04476049a6c5a3bda3e2
Kevin <metrix.28@gmail.com>
2020-11-02 14:02:33 -0500

    fix build

---
+++ docs/build/precache-manifest.d27e6c4de24c1f5dacb743edbe338388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689761780aa5ab21e9d2b91213196315",
+    "url": "/asap-rocky-quiz/index.html"
+  },
+  {
+    "revision": "239baf1292e582ea5512",
+    "url": "/asap-rocky-quiz/static/css/main.18868347.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/asap-rocky-quiz/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asap-rocky-quiz/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239baf1292e582ea5512",
+    "url": "/asap-rocky-quiz/static/js/main.67f60c19.chunk.js"
+  },
+  {
+    "revision": "4b11bc793a7cbabd2b8a",
+    "url": "/asap-rocky-quiz/static/js/runtime-main.d75af3ae.js"
+  },
+  {
+    "revision": "e1d39e0d6836eb7bf8726d76f44a319c",
+    "url": "/asap-rocky-quiz/static/media/rocky-lsd-wallpaper.e1d39e0d.jpg"
+  }
+]);
